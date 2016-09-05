e704f0d4d6a7eef8e54a817a7a0719bf50379976
kangussu <mendes.lk@gmail.com>
2016-09-05 12:42:25 -0300

    proj exemplo com um metodo de soma

---
+++ Teste1/Teste1/Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste1
+{
+    class Calc
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int intSoma (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
